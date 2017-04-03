d31ae23edb5523792e964c5091b9b67961f1b3d3
doubleface <christophe@thiriot.xyz>
2017-04-03 11:14:02 +0200

    Updates comming for cozy-konnector-template

--- models/bill.js
+++ models/bill.js
@@ -28,20 +28,3 @@ module.exports = {
     .catch(err => callback(err))
   }
 }
-
-// module.exports = Bill = cozydb.getModel('Bill', {
-//   type: String,
-//   subtype: String,
-//   date: Date,
-//   vendor: String,
-//   amount: Number,
-//   plan: String,
-//   pdfurl: String,
-//   binaryId: String,
-//   fileId: String,
-//   content: String,
-//   isRefund: Boolean,
-//   clientId: String, // Client number in vendor CRM
-//   number: String, // Bill Id, in vendor CRM
-//   docTypeVersion: String
-// }) // Document traceability : appName_Konnector-version

77d179d285162b7b0c11821b64d8b76aa8d2cd07
Olivier G <olivier@lespacedunmatin.info>
2017-04-13 16:39:09 +0200

    Create a sub folder into Bouygues to avoid conflict with the Bouygues Box konnector.

--- data/env.js
+++ data/env.js
@@ -4,5 +4,5 @@ const tokenPath = path.join(__dirname, 'token.json')
 module.exports = {
   COZY_CREDENTIALS: fs.existsSync(tokenPath) ? fs.readFileSync(tokenPath) : 'NO TOKEN',
   COZY_URL: 'http://cozy.tools:8080',
-  COZY_FIELDS: '{"phoneNumber": "0000000000", "password": "password", "folderPath": "Bouygues"}'
+  COZY_FIELDS: '{"phoneNumber": "0000000000", "password": "password", "folderPath": "Bouygues/telecom"}'
 }

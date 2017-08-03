be4b91893a80ec10ad77697df190e19e0137baf1
doubleface <christophe@thiriot.xyz>
2017-08-03 12:31:51 +0200

    feat: now use the last connector api

--- index.js
+++ index.js
@@ -1,12 +1,119 @@
-const konnector = require('./konnector')
-const {log} = require('cozy-konnector-libs')
+'use strict'
 
-const cozyFields = JSON.parse(process.env.COZY_FIELDS)
+const qs = require('querystring')
+const moment = require('moment')
 
-konnector.fetch({account: cozyFields.account, folderPath: cozyFields.folder_to_save}, err => {
-  log('debug', 'The konnector has been run')
-  if (err) {
-    log('error', err)
-    process.exit(1)
-  }
+const {log, BaseKonnector, saveBills, request} = require('cozy-konnector-libs')
+
+let rq = request({
+  cheerio: true,
+  json: false,
+  jar: true,
+  // debug: true,
+  headers: {}
 })
+
+module.exports = new BaseKonnector(function fetch (fields) {
+  return logIn(fields)
+  .then(parsePage)
+  .then(entries => saveBills(entries, fields.folderPath, {
+    timeout: Date.now() + 60 * 1000,
+    identifiers: 'bouyg'
+  }))
+  .catch(err => {
+    // Connector is not in error if there is not entry in the end
+    // It may be simply an empty account
+    if (err.message === 'NO_ENTRY') return []
+    throw err
+  })
+})
+
+// Procedure to login to Bouygues website.
+function logIn (fields) {
+  const loginUrl = 'https://www.mon-compte.bouyguestelecom.fr/cas/login'
+  const billUrl = 'https://www.bouyguestelecom.fr/parcours/mes-factures/historique'
+
+  log('info', 'Logging in on Bouygues Website...')
+  return rq(loginUrl)
+  .then($ => {
+    // Extract hidden values
+    const lt = $('input[name="lt"]').val()
+    const execution = $('input[name="execution"]').val()
+
+    // Second request to log in (post the form).
+    const form = {
+      'username': fields.login,
+      'password': fields.password,
+      'lt': lt,
+      'execution': execution,
+      '_eventId': 'submit'
+    }
+
+    const loginOptions = {
+      method: 'POST',
+      form,
+      uri: loginUrl
+    }
+    return rq(loginOptions)
+  })
+  .then($ => {
+    log('info', 'Successfully logged in.')
+    const badLogin = $('.error-icon').length > 0
+    if (badLogin) {
+      throw new Error('bad login page')
+    }
+  })
+  .then(() => {
+    log('info', 'Download bill HTML page...')
+    return rq(billUrl)
+  })
+  .catch(err => {
+    log('warning', err)
+    throw new Error('LOGIN_FAILED')
+  })
+}
+
+// Procedure to extract bill data from the page.
+function parsePage ($) {
+  let baseDlUrl = 'https://www.bouyguestelecom.fr/parcours/facture/download/index'
+  const entries = []
+  moment.locale('fr')
+
+  // We browse the bills table by processing each line one by one.
+  $('.download-facture').each(function () {
+    // Markup is not clean, we grab the date from the tag text.
+    let date = $(this).text()
+      .trim()
+      .split(' ')
+      .splice(0, 2)
+      .join(' ')
+      .trim()
+
+    // Amount is in a dirty field. We work on the tag text to extract data.
+    let amount = $(this).find('.small-prix').text().trim()
+    amount = amount.replace('€', ',').replace(',', '.')
+
+    // Get the facture id and build the download url from it.
+    const id = $(this).attr('facture-id')
+    const params = {id}
+    const fileurl = `${baseDlUrl}?${qs.stringify(params)}`
+    date = moment(date, 'MMMM YYYY').add(14, 'days')
+
+    // Build bill object.
+    const bill = {
+      date: date.toDate(),
+      amount,
+      fileurl,
+      filename: getFileName(date)
+    }
+    entries.push(bill)
+  })
+
+  log('info', 'Bill data parsed.')
+
+  return Promise.resolve(entries)
+}
+
+function getFileName (date) {
+  return `${date.format('YYYYMM')}_bouyguestelecom.pdf`
+}
